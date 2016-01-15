c89060b8154bfb4b9f6cc33499a7a04026d2e96f
ekolpakov <ekolpakov@greet-go.com>
2016-01-15 10:52:46 +0600

    Проверена работа GWT

--- greetgo.depinject.parent/greetgo.depinject.gwt.stand/src/kz/greetgo/depinject/gwt/stand/src/DepinjectStandEP.java
+++ greetgo.depinject.parent/greetgo.depinject.gwt.stand/src/kz/greetgo/depinject/gwt/stand/src/DepinjectStandEP.java
@@ -3,17 +3,21 @@ package kz.greetgo.depinject.gwt.stand.src;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class DepinjectStandEP implements EntryPoint {
   @Override
   public void onModuleLoad() {
-    
+
     TestBeanContainer tbc = GWT.create(TestBeanContainer.class);
-    
+
     AsdInterface asdInterface = tbc.getAsd();
     Window.alert("asdInterface = " + asdInterface);
-    
+
     tbc.getAsd().showHelloWorld();
 
+    HTML html = new HTML("Hello from GWT");
+    RootPanel.get().add(html);
   }
 }
