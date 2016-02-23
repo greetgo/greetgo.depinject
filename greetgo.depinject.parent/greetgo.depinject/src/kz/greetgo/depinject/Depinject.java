a29a0f4ebf800e942fd0fc1581379dd3c9a501e8
ekolpakov <ekolpakov@greet-go.com>
2016-02-23 17:29:45 +0600

    Добавлен метод DepinjectUtil.implementAndUseBeanContainers

--- greetgo.depinject.parent/greetgo.depinject/src/kz/greetgo/depinject/Depinject.java
+++ greetgo.depinject.parent/greetgo.depinject/src/kz/greetgo/depinject/Depinject.java
@@ -3,7 +3,7 @@ package kz.greetgo.depinject;
 import kz.greetgo.depinject.core.BeanContainer;
 
 public class Depinject {
-  public static <T extends BeanContainer> T newInstance(Class<T> tClass) throws Exception, InstantiationException {
+  public static <T extends BeanContainer> T newInstance(Class<T> tClass) throws Exception {
     //noinspection unchecked
     return (T) Class.forName(tClass.getName() + BeanContainer.IMPL_POSTFIX).newInstance();
   }
