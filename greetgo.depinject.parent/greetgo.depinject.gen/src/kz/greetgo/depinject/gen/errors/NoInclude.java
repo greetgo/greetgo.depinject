4f07ee9d95f4bcc91d742f2bf9128167a48f7359
ekolpakov <ekolpakov@greet-go.com>
2016-03-01 16:40:15 +0600

    Changed message in class NoInclude

--- greetgo.depinject.parent/greetgo.depinject.gen/src/kz/greetgo/depinject/gen/errors/NoInclude.java
+++ greetgo.depinject.parent/greetgo.depinject.gen/src/kz/greetgo/depinject/gen/errors/NoInclude.java
@@ -1,11 +1,11 @@
 package kz.greetgo.depinject.gen.errors;
 
 public class NoInclude extends RuntimeException {
-  
+
   public final Class<?> mustContainingClass;
-  
+
   public NoInclude(Class<?> mustContainingClass) {
-    super(" in " + mustContainingClass.getSimpleName());
+    super("In " + mustContainingClass.toString());
     this.mustContainingClass = mustContainingClass;
   }
 }
