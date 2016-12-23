9e8f3ad4f6757ae838c197782e7defc1cc32decb
ekolpakov <ekolpakov@greet-go.com>
2016-12-23 13:11:45 +0600

    Added left test

---
+++ greetgo.depinject.parent/greetgo.depinject.testng/test_src/kz/greetgo/depinject/testng/complex_tests/AsdTest.java
@@ -0,0 +1,10 @@
+package kz.greetgo.depinject.testng.complex_tests;
+
+import org.testng.annotations.Test;
+
+public class AsdTest {
+  @Test
+  public void test() throws Exception {
+    throw new RuntimeException("asd");
+  }
+}
