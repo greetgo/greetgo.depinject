946a0e46a207b1951f6a75fd61794f1dd7f94096
ekolpakov <ekolpakov@greet-go.com>
2017-06-02 11:59:14 +0600

    Added TestToTestTestAlarm

---
+++ greetgo.depinject.parent/greetgo.depinject.gen/test_src/kz/greetgo/depinject/gen/TestToTestTestAlarm.java
@@ -0,0 +1,11 @@
+package kz.greetgo.depinject.gen;
+
+import org.testng.annotations.Test;
+
+public class TestToTestTestAlarm {
+
+  @Test
+  public void alarm() throws Exception {
+    throw new Exception("alarm");
+  }
+}
