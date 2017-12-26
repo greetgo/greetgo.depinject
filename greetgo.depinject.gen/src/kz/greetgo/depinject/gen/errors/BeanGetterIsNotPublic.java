a1e64d0e32a61055af550d5fb361c007c97b37e4
ekolpakov <ekolpakov@greet-go.com>
2017-12-26 15:03:29 +0600

    Расширено сообщение в BeanGetterIsNotPublic

--- greetgo.depinject.gen/src/kz/greetgo/depinject/gen/errors/BeanGetterIsNotPublic.java
+++ greetgo.depinject.gen/src/kz/greetgo/depinject/gen/errors/BeanGetterIsNotPublic.java
@@ -9,7 +9,8 @@ public class BeanGetterIsNotPublic extends RuntimeException {
 
   public BeanGetterIsNotPublic(Class<?> containsFieldClass, Field beanGetterField, Class<?> beanClass) {
     super("Field " + beanGetterField.getName() + " is not public in "
-      + containsFieldClass + (beanClass == null ? "" : ": it is the parent of bean " + beanClass));
+      + containsFieldClass + (beanClass == null ? "" : ": it is the parent of bean " + beanClass) + ".\n" +
+      "\tIf you want none public BeanGetter field you may add annotation @LetBeNonePublic to field or class");
     this.containsFieldClass = containsFieldClass;
     this.beanGetterField = beanGetterField;
     this.beanClass = beanClass;
