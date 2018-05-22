b2daf68fb9b61330eca04a26be7297d266be79a0
ekolpakov <ekolpakov@greet-go.com>
2018-05-22 15:32:54 +0600

    Исправлен касяк javadoc у GWT

--- greetgo.depinject.gwt/src/kz/greetgo/depinject/gwt/src/SyncAsyncConverter.java
+++ greetgo.depinject.gwt/src/kz/greetgo/depinject/gwt/src/SyncAsyncConverter.java
@@ -4,6 +4,8 @@ import com.google.gwt.http.client.Request;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface SyncAsyncConverter {
-  public <ToServer, FromServer> Request convertInvoking(final ToServer toServer,
-                                                        final AsyncCallback<FromServer> callback, InvokeService<ToServer, FromServer> sync);
+  <ToServer, FromServer>
+  Request convertInvoking(ToServer toServer,
+                          AsyncCallback<FromServer> callback,
+                          InvokeService<ToServer, FromServer> sync);
 }
