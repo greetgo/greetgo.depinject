6b97da1972bdeffaf001ef28c1b2cf41170e58a9
ekolpakov <ekolpakov@greet-go.com>
2018-07-12 13:05:14 +0600

    Added empty gradle plugin

---
+++ greetgo.depinject.gradle/src_groovy/kz/greetgo/depinject/gradle/DepinjectPlugin.groovy
@@ -0,0 +1,11 @@
+package kz.greetgo.depinject.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class DepinjectPlugin implements Plugin<Project> {
+  @Override
+  void apply(Project target) {
+
+  }
+}
---
+++ greetgo.depinject.gradle/test_src_groovy/kz/greetgo/depinject/gradle/DepinjectPluginTest.groovy
@@ -0,0 +1,10 @@
+package kz.greetgo.depinject.gradle
+
+import org.testng.annotations.Test
+
+class DepinjectPluginTest {
+  @Test
+  void "Это супер пупер навороченный тест"() {
+    println "asd"
+  }
+}
