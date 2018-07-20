2b96f048f0e980f48deda92bfdf4491fae2da0e4
ekolpakov <ekolpakov@greet-go.com>
2018-07-20 18:09:51 +0600

    Работааааает!!!!

--- greetgo.depinject.gradle/src_groovy/kz/greetgo/depinject/gradle/DepinjectPlugin.groovy
+++ greetgo.depinject.gradle/src_groovy/kz/greetgo/depinject/gradle/DepinjectPlugin.groovy
@@ -2,6 +2,7 @@ package kz.greetgo.depinject.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.tasks.JavaExec
 
 class DepinjectPlugin implements Plugin<Project> {
@@ -15,13 +16,13 @@ class DepinjectPlugin implements Plugin<Project> {
     }
 
     project.task('generate', type: JavaExec) {
+      doFirst {
+        def javaPluginConvention = project.getConvention().getPlugin(JavaPluginConvention)
+        classpath javaPluginConvention.getSourceSets().findByName("test").getRuntimeClasspath()
+      }
+
       main = 'kz.greetgo.depinject.gen.DepinjectGenerate'
       args = ['impl', '-p', 'kz.greetgo.tests', '-s', project.buildDir.getAbsolutePath() + "/depinject_generated_src"]
-      classpath project.sourceSets.test.runtimeClasspath
-
-      doFirst {
-        println "---------------------> " + project.configurations.testCompile
-      }
     }
 
   }
