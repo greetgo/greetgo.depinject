fa16c62180a3484358cfe29dedcc839843cfa6fc
ekolpakov <ekolpakov@greet-go.com>
2018-10-08 17:50:15 +0600

    Under Kotlin is working when kotlin in another module then depinject.plugin is working

---
+++ investigation/kotlin_probe/module2/src/kz/greetgo/kotlin_probe/module2/Bean2.kt
@@ -0,0 +1,10 @@
+package kz.greetgo.kotlin_probe.module2
+
+import kz.greetgo.depinject.core.Bean
+
+@Bean
+class Bean2 {
+  fun printHelloWorld() {
+    println("Hello World from " + javaClass.simpleName + " (under kotlin!!!)")
+  }
+}
