2f708b3239ab52d75004c036187a9dc81c25aa57
ekolpakov <ekolpakov@greet-go.com>
2019-08-21 10:48:28 +0600

    Восстановление старых файлов после крупного рефакторинга

---
+++ depinject/annotations/src/kz/greetgo/depinject/ann/HideFromDepinject.java
@@ -0,0 +1,20 @@
+package kz.greetgo.depinject.ann;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p>
+ * Hides constructor from depinject
+ * </p>
+ * <p>
+ * If you do not want depinject used any constructor to create bean, then mark the constructor by this annotation
+ * </p>
+ */
+@Documented
+@Target({ElementType.CONSTRUCTOR})
+@Retention(RetentionPolicy.SOURCE)
+public @interface HideFromDepinject {}
